99d505875de28423d9ec4288d2965ea1e66d9dfb
wernikeyd <wernikeyd@gmail.com>
2025-12-24 05:24:32 +0300

    Добавлены два новых метода takeDamage, attack и геттер getName

--- rpg/main.cpp
+++ rpg/main.cpp
@@ -20,10 +20,27 @@ public:
 
     // Метод для получения информации
     void getInfo() {
-        cout << name << " (HP: " << health << ", Урон: " << damage << ")" << endl;
-    }
-};
+        cout << name << " (HP: " << health << ", Урон: " << damage << ")" << endl;}
 
+    // метод: получение урона
+    void takeDamage(int dmg) {
+        health -= dmg;
+        if (health < 0) health = 0;
+        cout << name << " получил " << dmg << " урона. Осталось HP: " << health << endl;
+    }
+
+    //  метод: атака другого персонажа
+    void attack(Character* target) {
+        cout << name << " атакует " << target->name << "!" << endl;
+        target->takeDamage(damage);
+    }
+
+    // Геттер для имени
+    string getName() {
+        return name;
+    }
+
+};
 int main() {
     return 0;
 }
